2f9136f9777be271154561207e4821ba95072726
rigole <foplacide@gmail.com>
2020-12-17 02:29:22 +0100

    about and contact page

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name = 'home'),
+    path('about/', views.AboutPageView.as_view(), name = 'about'),
+    path('contact/', views.ContactPageView.as_view(), name = 'contact'),
 ]
